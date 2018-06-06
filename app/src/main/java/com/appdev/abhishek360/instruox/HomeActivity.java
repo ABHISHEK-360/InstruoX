d92f16adb00c05cf4e55bb1c305b5a0ac55f0f12
abhishek-360 <abhi.kumar310@gmail.com>
2018-06-06 07:12:01 +0530

    google signIn login popup visbility without login

--- app/src/main/java/com/appdev/abhishek360/instruox/HomeActivity.java
+++ app/src/main/java/com/appdev/abhishek360/instruox/HomeActivity.java
@@ -1,8 +1,10 @@
 package com.appdev.abhishek360.instruox;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,31 +14,84 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity
-{
+import com.bumptech.glide.Glide;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     ActionBarDrawerToggle actionBarDrawerToggle;
     DrawerLayout drawerLayout;
+    TextView nameView,emailView;
+    FloatingActionButton floatingActionButton;
+    CircleImageView proPic;
+    ImageView logoView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.action_bar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
 
+        String name = getIntent().getExtras().getString("name");
+        String email = getIntent().getExtras().getString("email");
+        String imgUrl = getIntent().getExtras().getString("Url");
+
+
+
+
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        View header = navigationView.getHeaderView(0);
+
         actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close);
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
+         nameView = (TextView)header.findViewById(R.id.hname_user);
+
+         emailView = (TextView)header.findViewById(R.id.hemail);
+
+         floatingActionButton = (FloatingActionButton)header.findViewById(R.id.SearchPic);
+
+        proPic = (CircleImageView)header.findViewById(R.id.ProfilePic);
+        logoView =(ImageView)header.findViewById(R.id.logoView);
+        proPic.setVisibility(View.GONE);
+
+        if(name.equals("alpha")&&email.equals("alpha@base"))
+        {
+            nameView.setText("INSTRUO-2018");
+            emailView.setText("The 10th Edition!");
+            proPic.setImageResource(R.drawable.instruo_logo);
+            floatingActionButton.setVisibility(View.GONE);
+
+        }
+        else
+        {
+            nameView.setText(name);
+            emailView.setText(email);
+            Glide.with(this).load(imgUrl).into(proPic);
+
+            logoView.setVisibility(View.GONE);
+            proPic.setVisibility(View.VISIBLE);
+
+        }
+
+
 
     }
 
@@ -58,5 +113,10 @@ public class HomeActivity extends AppCompatActivity
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
--- app/src/main/java/com/appdev/abhishek360/instruox/LoginActivity.java
+++ app/src/main/java/com/appdev/abhishek360/instruox/LoginActivity.java
@@ -1,21 +1,144 @@
 package com.appdev.abhishek360.instruox;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener,GoogleApiClient.OnConnectionFailedListener
+{
+    private TextView username,pass;
+    private Button signIn,regi;
+    private static final int REQUEST_CODE=9001;
+    private GoogleApiClient googleApiClient;
+    String name="alpha",email="alpha@base",imgURL="alpha.com";
+
+    Dialog myDailog;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        myDailog = new Dialog(this);
+
+    }
+    public void showPopUp(View V)
+    {
+        myDailog.setContentView(R.layout.custompopup);
+        username = (TextView)findViewById(R.id.username);
+        pass = (TextView)findViewById(R.id.Password);
+        signIn = (Button)findViewById(R.id.signIn);
+        regi = (Button)findViewById(R.id.regi);
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,signInOptions).build();
+
+
+        FloatingActionButton closeBtn = (FloatingActionButton) myDailog.findViewById(R.id.closeDialog);
+
+
+        closeBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                myDailog.dismiss();
+            }
+        });
+
+        myDailog.show();
+
+
     }
 
-    public void skipLogin(View view) {
+    public void onClick(View V)
+    {
+        switch( V.getId())
+        {
+            case R.id.gSignIn:
+                signIN();
+                break;
+
+
+
+        }
+    }
+
+    private void signIN()
+    {
+        Intent i =  Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(i,REQUEST_CODE);
+
+
+    }
+
+    private void signOut()
+    {
+
+    }
+
+    private void onRequest(GoogleSignInResult result)
+    {
+        if (result.isSuccess())
+        {
+            GoogleSignInAccount account = result.getSignInAccount();
+             name = account.getDisplayName();
+             email = account.getEmail();
+             imgURL = account.getPhotoUrl().toString();
+            Intent in = new Intent(LoginActivity.this,HomeActivity.class);
+            in.putExtra("name",name);
+            in.putExtra("email",email);
+            in.putExtra("Url",imgURL);
+            startActivity(in);
+
+
+        }
+    }
+
+    public void skipLogin(View view)
+    {
         Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+        intent.putExtra("name",name);
+        intent.putExtra("email",email);
+        intent.putExtra("Url",imgURL);
+
         startActivity(intent);
+
+
+    }
+
+    private void changeUI(boolean isLogin)
+    {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQUEST_CODE)
+        {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            onRequest(result);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
+    {
 
     }
 }
